bd8af503eeeb8e1d4831e29fb8083953f762734e
Golan <golan@Golanss-MacBook-Pro.local>
2016-07-03 15:15:43 +0300

    new version of AppsFlyer Unity example

---
+++ Assets/Plugins/AppsFlyerTrackerCallbacks.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AppsFlyerTrackerCallbacks : MonoBehaviour {
+	public GameObject text;
+	// Use this for initialization
+	void Start () {
+		print ("AppsFlyerTrackerCallbacks on Start");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void didReceiveConversionData(string conversionData) {
+		print ("AppsFlyerTrackerCallbacks:: got conversion data = " + conversionData);
+		if (conversionData.Contains ("Non")) {
+			text.GetComponent<Text> ().text = "Non-Organic Install";
+		} else {
+			text.GetComponent<Text> ().text = "Organic Install";
+		}
+	}
+	
+	public void didReceiveConversionDataWithError(string error) {
+		print ("AppsFlyerTrackerCallbacks:: got conversion data error = " + error);
+	}
+	
+	public void didFinishValidateReceipt(string validateResult) {
+		print ("AppsFlyerTrackerCallbacks:: got didFinishValidateReceipt  = " + validateResult);
+		
+	}
+	
+	public void didFinishValidateReceiptWithError (string error) {
+		print ("AppsFlyerTrackerCallbacks:: got idFinishValidateReceiptWithError error = " + error);
+		
+	}
+	
+	public void onAppOpenAttribution(string validateResult) {
+		print ("AppsFlyerTrackerCallbacks:: got onAppOpenAttribution  = " + validateResult);
+		
+	}
+	
+	public void onAppOpenAttributionFailure (string error) {
+		print ("AppsFlyerTrackerCallbacks:: got onAppOpenAttributionFailure error = " + error);
+		
+	}
+	
+	public void onInAppBillingSuccess () {
+		print ("AppsFlyerTrackerCallbacks:: got onInAppBillingSuccess succcess");
+		
+	}
+	public void onInAppBillingFailure (string error) {
+		print ("AppsFlyerTrackerCallbacks:: got onInAppBillingFailure error = " + error);
+		
+	}
+}
